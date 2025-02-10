b69ebf645be9fd136de0896c136ff75bdba9e6f3
tamerelateeq <tamerelateeq@gmail.com>
2025-02-10 18:16:10 +0200

    add sliding animation text

--- lib/presentation/Views/Widgets/splach_view_body.dart
+++ lib/presentation/Views/Widgets/splach_view_body.dart
@@ -1,8 +1,28 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/presentation/Views/Widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplachViewBody extends StatelessWidget {
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
+
+  @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    slidingAnimation = Tween(begin: Offset(0, 2), end: Offset.zero)
+        .animate(animationController);
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +36,8 @@ class SplachViewBody extends StatelessWidget {
         ),
         // add space bysize box
         const SizedBox(height: 4),
-        // add text  'Read Free Books'
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        )
+        // add slideing animation text
+        Sliding_text(slidingAnimation: slidingAnimation),
       ],
     );
   }

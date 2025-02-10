5e76f2865a9dedf53fd78e4503bf5d0435c17472
tamerelateeq <tamerelateeq@gmail.com>
2025-02-10 15:01:48 +0200

    add splach view

---
+++ lib/presentation/Views/splach_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplachView extends StatelessWidget {
+  const SplachView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

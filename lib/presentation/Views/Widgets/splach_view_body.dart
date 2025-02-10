be4cde24cbfcdba8b7cffa7303704d30405b8ddb
tamerelateeq <tamerelateeq@gmail.com>
2025-02-10 17:30:51 +0200

    add assets class to utils and add logo

--- lib/presentation/Views/Widgets/splach_view_body.dart
+++ lib/presentation/Views/Widgets/splach_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class SplachViewBody extends StatelessWidget {
@@ -5,6 +6,12 @@ class SplachViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Image.asset(
+          AssetsData.logo,
+        )
+      ],
+    );
   }
 }

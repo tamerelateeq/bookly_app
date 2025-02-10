e80eb54d65e822398d9c7eb29ae86c535b367437
tamerelateeq <tamerelateeq@gmail.com>
2025-02-10 15:03:12 +0200

    add splach view body to splach view

---
+++ lib/presentation/Views/Widgets/splach_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplachViewBody extends StatelessWidget {
+  const SplachViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/presentation/Views/splach_view.dart
+++ lib/presentation/Views/splach_view.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/presentation/Views/Widgets/splach_view_body.dart';
 import 'package:flutter/material.dart';
 
 class SplachView extends StatelessWidget {
@@ -5,6 +6,8 @@ class SplachView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SplachViewBody(),
+    );
   }
 }

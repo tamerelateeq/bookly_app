ae2ecd4e106f420afc526c92abfc921ee3220588
tamerelateeq <tamerelateeq@gmail.com>
2025-02-14 22:13:16 +0200

    add serach appbar

---
+++ lib/Features/search/views/widgets/search_appbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchAppbar extends StatelessWidget {
+  const SearchAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+        //TextField
+
+        );
+  }
+}

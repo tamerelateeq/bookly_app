631890ddccc4b03fc3f13b87bc15804397477089
tamerelateeq <tamerelateeq@gmail.com>
2025-02-14 22:14:18 +0200

    add custome text field

---
+++ lib/core/widgets/custome_textfield.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomeTextfield extends StatelessWidget {
+  const CustomeTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}

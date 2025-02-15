25cbe7279521365e69d0bd484936ad78f4729dd4
tamerelateeq <tamerelateeq@gmail.com>
2025-02-15 18:49:08 +0200

    cretae home repo method

---
+++ lib/Features/home/data/model/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:bookly_app/Features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/core/utils/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failures, List<BookModel>>> fetchBestSellarBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturesBooks();
+}

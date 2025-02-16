a784423628e01da22e6e66b3dd185683862075e6
tamerelateeq <tamerelateeq@gmail.com>
2025-02-16 17:29:35 +0200

    create featured books method

--- lib/Features/home/presentation/manger/cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/cubit/featured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/model/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  // add repo
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    // access fetch fetaure books from repo & store it in result
+    var result = await homeRepo.fetchFeaturesBooks();
+    // fold is function to get two condation
+    result.fold((failure) {
+      emit(FeaturedBooksFailure(failure.errMassage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }

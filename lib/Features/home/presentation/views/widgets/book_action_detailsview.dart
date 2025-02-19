1a3b9cd8bf82d686f04eee5d4746a482f2e1772e
tamerelateeq <tamerelateeq@gmail.com>
2025-02-19 19:11:34 +0200

    final details view book

--- lib/Features/home/presentation/views/widgets/book_action_detailsview.dart
+++ lib/Features/home/presentation/views/widgets/book_action_detailsview.dart
@@ -1,7 +1,9 @@
 import 'package:bookly_app/Features/home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/core/utils/function/launch_url.dart';
 import 'package:bookly_app/core/widgets/custome_button.dart';
+import 'package:bookly_app/core/widgets/snake_massage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BookActions extends StatelessWidget {
   const BookActions({
@@ -35,14 +37,21 @@ class BookActions extends StatelessWidget {
           // Button for free
           Expanded(
             child: CustomeButton(
-              onTap: () {},
+              onTap: () {
+                showCustomeSnackbar(
+                  context,
+                  'Please Download Adobe Digital Editions program ',
+                );
+                cutomeLaunchUrl(
+                    context, bookModel.accessInfo!.pdf!.acsTokenLink!);
+              },
               colorButton: Color(0xffef8262),
               colorText: Colors.white,
               borderRadius: BorderRadius.only(
                 bottomRight: Radius.circular(16),
                 topRight: Radius.circular(16),
               ),
-              text: 'Free preview',
+              text: 'Download',
             ),
           ),
         ],
--- lib/core/utils/function/launch_url.dart
+++ lib/core/utils/function/launch_url.dart
@@ -4,6 +4,8 @@ import 'package:url_launcher/url_launcher.dart';
 Future<void> cutomeLaunchUrl(context, String url) async {
   Uri uri = Uri.parse(url);
   if (await launchUrl(uri)) {
+    showCustomeSnackbar(context, 'Enjoy your Read 😊');
+  } else {
     showCustomeSnackbar(context, 'Opps Linke not found');
   }
 }
